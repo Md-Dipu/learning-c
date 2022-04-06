4175917caf8fc27bddab37ebdfed050460ed068e
Md. Dipu <srabun.tangail@gmail.com>
2022-04-06 17:35:51 +0600

    [update]mad libs

--- Day-01/mad-libs.c
+++ Day-01/mad-libs.c
@@ -3,17 +3,30 @@
 int main()
 {
     char name[20];
-    int age;
+    char color[20];
+    int favorite_number;
+    char celebrity[20];
 
     // getting name
     printf("Enter your name: ");
-    scanf("%s", name);
+    scanf("%[^\n]", name);
 
-    // getting age
-    printf("Enter your age: ");
-    scanf("%d", &age);
+    // getting color
+    printf("Enter a color: ");
+    scanf("\n");
+    scanf("%[^\n]", color);
 
-    printf("My name is %s. My age is %d.", name, age);
+    // getting favorite number
+    printf("Enter a number: ");
+    scanf("%d", &favorite_number);
+
+    // getting celebrity name
+    printf("Enter a celebrity name: ");
+    scanf("\n");
+    scanf("%[^\n]", celebrity);
+
+    // printing
+    printf("\nMy name is %s. \nI favorite color is %s. \nMy favorite number is %d. \nI love %s.", name, color, favorite_number, celebrity);
 
     return 0;
 }
