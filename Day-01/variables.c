f12cd77b2e9b79c4fa7333f45a6ff064b4ba5146
Md. Dipu <srabun.tangail@gmail.com>
2022-04-06 12:38:25 +0600

    [add]variables

---
+++ Day-01/variables.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int variable_name = 0;
+
+    float grade_point = 3.1;
+    grade_point = 8.4;
+
+    char grade = 'A';
+
+    char name[] = "Sadman";
+
+    printf("%s", name);
+    return 0;
+}
