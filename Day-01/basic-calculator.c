16d0f5b499fac34201a5bf62fb0d3a4269998c74
Md. Dipu <srabun.tangail@gmail.com>
2022-04-13 12:07:28 +0600

    [add]basic calculator(adder)

---
+++ Day-01/basic-calculator.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2, sum;          // variable declearing here
+    scanf("%d %d", &num1, &num2); // user input
+
+    sum = num1 + num2; // processing (sum)
+
+    printf("\nResult is %d", sum); // output
+    return 0;
+}
