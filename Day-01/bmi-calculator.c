83cc1493a7603da6f853a3a9d38a38a98f42dc2b
Md. Dipu <srabun.tangail@gmail.com>
2022-04-13 12:07:54 +0600

    [add]bmi calculator

---
+++ Day-01/bmi-calculator.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    float weight, height_foot, height_inch;
+
+    printf("Enter your weight: ");
+    scanf("%f", &weight);
+
+    printf("Enter your height: ");
+    scanf("%f'%f\"", &height_foot, &height_inch);
+
+    height_foot += height_inch / 12;
+    float height = height_foot * 0.3048; // converting foot to meter
+    float bmi = weight / (height * height);
+
+    printf("Your BMI is %f", bmi);
+    return 0;
+}
