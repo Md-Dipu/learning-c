3d96b566060342b59732fac37d7a28290928697e
Md. Dipu <srabun.tangail@gmail.com>
2022-04-14 16:32:28 +0600

    [add]switch

---
+++ Day-02/switch.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    // int/char
+    char ch = 'A';
+
+    switch (/* expression */)
+    {
+    case /* constant-expression */:
+        /* code */
+        break;
+
+    default:
+        break;
+    }
+
+    return 0;
+}
