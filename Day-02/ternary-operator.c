f99fa00daab48040d1f829c4af657fde5292625b
Md. Dipu <srabun.tangail@gmail.com>
2022-04-14 16:33:24 +0600

    [add]ternary operator

---
+++ Day-02/ternary-operator.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    // if (0 < 1)
+    //     printf("0 is less than 1.");
+    // else
+    //     printf("0 is not less than 1.");
+
+    (0 < 1) ? printf("0 is less than 1.") : printf("0 is not less than 1.");
+    // cond ? if cond is true : if cond is false;
+
+    return 0;
+}
