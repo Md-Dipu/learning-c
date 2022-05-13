fa52a5da140863f1a11e4fffa269e243042585b4
Md. Dipu <srabun.tangail@gmail.com>
2022-05-13 22:13:06 +0600

    [add] calculator

---
+++ Day-3/custom-libary/calculator.h
@@ -0,0 +1,26 @@
+#ifndef __CALC_LIB
+#define __CALC_LIB
+
+/**
+ * This a adder funcion. Take two intger number as
+ * parameter and return sum is also a intger number.
+ */
+int adder(int x, int y);
+
+/**
+ * This a subtractor funcion. Take two intger number as
+ * parameter x, y and return difference from x to y.
+ */
+int subtractor(int x, int y);
+
+/**
+ * This a multiplier funcion. The function return intger.
+ */
+int multiplier(int x, int y);
+
+/**
+ * This a divider funcion. The function return float.
+ */
+float divider(float x, float y);
+
+#endif //__CALC_LIB
