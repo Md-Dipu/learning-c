e83d7dd4b0b1dab294706f0f7c28855ba7cd8307
Md. Dipu <srabun.tangail@gmail.com>
2022-06-05 23:41:27 +0600

    [add] complier and logical error

---
+++ Day-1/error.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    // printf("This is a complier time error")
+    // printf(its a compiler time error);
+
+    int a = 0;
+    int b = 4;
+    // printf("Divided: %d", b / a);
+
+    int num1 = 8, num2 = 2;
+    printf("Div is %d", num1 % num2); // logical error
+
+    return 0;
+}
