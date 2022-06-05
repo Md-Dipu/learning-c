cdb6a51d637d15cad88345c1ba22c7f8ae249ffd
Md. Dipu <srabun.tangail@gmail.com>
2022-06-05 23:49:46 +0600

    [add] while nested loop

---
+++ Day-3/nested-loop-with-while.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 1;
+    while (i <= 10)
+    {
+        int j = 1;
+        while (j <= 10)
+        {
+            printf("%d X %d = %d\n", i, j, i * j);
+            j++;
+        }
+
+        printf("\n");
+        i++;
+    }
+    return 0;
+}
