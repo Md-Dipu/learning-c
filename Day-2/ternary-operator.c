989446fffae7bb8de4d5bc1c3770cbc0f7d808e0
Md. Dipu <srabun.tangail@gmail.com>
2022-06-05 23:55:16 +0600

    [update] ternary operator

--- Day-2/ternary-operator.c
+++ Day-2/ternary-operator.c
@@ -2,13 +2,35 @@
 
 int main()
 {
-    // if (0 < 1)
-    //     printf("0 is less than 1.");
+    // if (2 < 3)
+    // {
+    //     printf("2 is less than 3");
+    // }
     // else
-    //     printf("0 is not less than 1.");
+    // {
+    //     printf("2 is not less than 3");
+    // }
 
-    (0 < 1) ? printf("0 is less than 1.") : printf("0 is not less than 1.");
-    // cond ? if cond is true : if cond is false;
+    // ?:
+
+    // (2 == 3) ? printf("2 is equal to 3") : printf("2 is not equal to 3");
+
+    int a, b, c;
+    printf("Enter 1st number: ");
+    scanf("%d", &a);
+    printf("Enter 2nd number: ");
+    scanf("%d", &b);
+    printf("Enter 3rd number: ");
+    scanf("%d", &c);
+
+    int max = (a >= b && a >= c) ? a : (b >= c) ? b
+                                                : c;
+
+    // as condition 1: if a is bigger than b and c
+    // true: max = a
+    // false: max = (b > c) ? b : c;
+
+    printf("%d is bigger", max);
 
     return 0;
 }
