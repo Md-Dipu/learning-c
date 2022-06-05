0888ab9a1d5345234d7a25d5da906b7927b8917d
Md. Dipu <srabun.tangail@gmail.com>
2022-06-05 23:41:57 +0600

    [add] std output

---
+++ Day-1/std-o.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int n = 20;
+    float salary = 35000.0;
+    float pi = 3.1416;
+    char grade = 'A';
+    char friend_name[] = "Sakib";
+
+    // printf("I am a c progarmmer. %s", friend_name);
+    return 0;
+}
