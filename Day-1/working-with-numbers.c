ca36cbbbef8d1b1ca456add86735435f61d89c13
Md. Dipu <srabun.tangail@gmail.com>
2022-06-05 23:45:15 +0600

    [add] use of operator

---
+++ Day-1/working-with-numbers.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 5, b = 3;
+
+    // printf("%d\n", a + b);
+    // printf("%d\n", a - b);
+    // printf("%d\n", a * b);
+    // printf("%d\n", a / b);
+    // printf("%d\n", a % b);
+
+    // a += 2; // a = a + 2;
+    // b -= 1; // b = b - 1;
+
+    // a = +3;
+    // b = -3;
+
+    // a++; // a = a + 1;
+    // b--; // b = b - 1;
+
+    // ++a;
+    // --b;
+
+    printf("%d\n", ++a);
+    printf("%d\n", ++b);
+
+    printf("%d\n", a);
+    printf("%d\n", b);
+
+    return 0;
+}
